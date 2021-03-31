a2074d26353c496edfb614ae59eb62e6f3c71af1
NamiKaze7 <57282567+NamiKaze7@users.noreply.github.com>
2021-03-31 15:50:49 +0800

    'new'

--- tag_op/test.py
+++ tag_op/test.py
@@ -6,9 +6,7 @@ with open("tag_op/data/roberta/tagop_roberta_cached_train.pkl", 'rb') as f:
 idex = 0
 
 for item in data:
-    print(item["span_pos_labels"][0])
-    print(item["input_ids"])
-    print('----------------------------')
-    idex += 1
-    if idex > 2:
-        break
+    if item["operator_label"] <= 1:
+       if item["span_pos_labels"][0] == -1:
+           print(item["answer_dict"])
+
