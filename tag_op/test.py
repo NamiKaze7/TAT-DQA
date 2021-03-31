ca118f6c4ec9621d4fa9c9865c851ecc0ce475ec
NamiKaze7 <57282567+NamiKaze7@users.noreply.github.com>
2021-03-31 15:08:43 +0800

    'new'

---
+++ tag_op/test.py
@@ -0,0 +1,13 @@
+import pickle
+
+with open("tag_op/data/roberta/tagop_roberta_cached_train.pkl", 'rb') as f:
+    data = pickle.load(f)
+
+idex = 0
+
+for item in data:
+    print(data["span_pos_labels"])
+    print('----------------------------')
+    idex += 1
+    if idex > 9:
+        break
