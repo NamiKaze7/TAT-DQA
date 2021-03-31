a4e47abb7393035ea3a5cecebd1acb1f46ecbfd3
NamiKaze7 <57282567+NamiKaze7@users.noreply.github.com>
2021-03-31 15:51:41 +0800

    'new'

--- tag_op/test.py
+++ tag_op/test.py
@@ -7,6 +7,6 @@ idex = 0
 
 for item in data:
     if item["operator_label"] <= 1:
-       if item["span_pos_labels"][0] == -1:
+       if item["span_pos_labels"][0][0] == -1:
            print(item["answer_dict"])
 
