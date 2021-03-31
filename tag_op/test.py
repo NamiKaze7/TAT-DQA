b1deaa86046a4efc15b49831d8fda9540e2ecfca
NamiKaze7 <57282567+NamiKaze7@users.noreply.github.com>
2021-03-31 15:58:37 +0800

    'new'

--- tag_op/test.py
+++ tag_op/test.py
@@ -4,9 +4,10 @@ with open("tag_op/data/roberta/tagop_roberta_cached_train.pkl", 'rb') as f:
     data = pickle.load(f)
 
 idex = 0
-
+total = 0
 for item in data:
     if item["operator_label"] <= 1:
+        total += 1
         if item["span_pos_labels"][0][0] == -1:
             print("-------------------------------")
             print("index :{}".format(idex))
@@ -16,3 +17,4 @@ for item in data:
             print(item["answer_dict"])
 
             idex += 1
+print("total: %d" % total)
