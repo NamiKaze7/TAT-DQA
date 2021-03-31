b7b061b6258c6a2229af9f27895a5fb12abf6ec7
NamiKaze7 <57282567+NamiKaze7@users.noreply.github.com>
2021-03-31 15:10:41 +0800

    'new'

--- tag_op/test.py
+++ tag_op/test.py
@@ -6,7 +6,7 @@ with open("tag_op/data/roberta/tagop_roberta_cached_train.pkl", 'rb') as f:
 idex = 0
 
 for item in data:
-    print(item["span_pos_labels"])
+    print(item["span_pos_labels"][0])
     print('----------------------------')
     idex += 1
     if idex > 9:
